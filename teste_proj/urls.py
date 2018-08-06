8c4d6aab7313deca56079d9df76d5079b6023a4e
Victor Estauti <estautib@gmail.com>
2018-08-05 22:49:27 -0300

    changed login/logout view call on url according to django 2.1

--- teste_proj/urls.py
+++ teste_proj/urls.py
@@ -20,6 +20,6 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("pessoas.urls")),
-    path('login/', auth_views.login, name='login'),
-    path('logout/', auth_views.logout, name='logout'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
