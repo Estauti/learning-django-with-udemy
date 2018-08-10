4dbca5a38cc2fafef016171f5f667c80a9bc02a9
Victor Estauti <estautib@gmail.com>
2018-08-10 19:37:55 -0300

    installed missing packages, made some minor bugs to model and made migrations

--- pessoas/models.py
+++ pessoas/models.py
@@ -3,19 +3,19 @@ from django.conf import settings
 
 # Create your models here.
 
-class Person(models.Model):
-    name = models.CharField(max_length=80)
-    height = models.DecimalField(max_digits=4, decimal_places=2)
-    birth_date = models.DateField()
-    photo = models.ImageField(upload_to='fotos_pessoas')
-    document = models.OneToOneField(Person, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.name
-
 class Document(models.Model):
     identification = models.CharField(max_length=30)
     date_reg = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.identification
+
+class Person(models.Model):
+    name = models.CharField(max_length=80)
+    height = models.DecimalField(max_digits=4, decimal_places=2, blank=True, null=True)
+    birth_date = models.DateField()
+    photo = models.ImageField(upload_to='fotos_pessoas')
+    document = models.OneToOneField(Document, on_delete=models.CASCADE, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
