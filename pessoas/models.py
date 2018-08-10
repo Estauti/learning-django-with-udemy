c449b8ecb1ad58e619021c63b8690c261936bae2
Victor Estauti <estautib@gmail.com>
2018-08-10 19:30:26 -0300

    included Document model

--- pessoas/models.py
+++ pessoas/models.py
@@ -4,10 +4,18 @@ from django.conf import settings
 # Create your models here.
 
 class Person(models.Model):
-    nome_completo = models.CharField(max_length=80)
-    idade = models.PositiveIntegerField()
-    data_de_nascimento = models.DateField()
-    foto = models.ImageField(upload_to='fotos_pessoas')
+    name = models.CharField(max_length=80)
+    height = models.DecimalField(max_digits=4, decimal_places=2)
+    birth_date = models.DateField()
+    photo = models.ImageField(upload_to='fotos_pessoas')
+    document = models.OneToOneField(Person, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.nome_completo
+        return self.name
+
+class Document(models.Model):
+    identification = models.CharField(max_length=30)
+    date_reg = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.identification
